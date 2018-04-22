013fc67047a21095ee09486908777e9f4c233e9f
DmitriyBatey <deman632011@gmail.com>
2018-04-22 20:23:28 +0400

    :)

--- CourseWorkonDB/src/main/java/DAO/BusesService.java
+++ CourseWorkonDB/src/main/java/DAO/BusesService.java
@@ -1,6 +1,6 @@
 package DAO;
 
-import Beans.AutocompaniesBean;
+import Beans.BusesBean;
 import ConnectionToDB.SingletoneConnection;
 
 import java.sql.Connection;
@@ -9,20 +9,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class AutocompaniesService implements AutocompaniesDAO {
+public class BusesService implements BusesDAO {
 
     @Override
-    public ArrayList<AutocompaniesBean> getAll() {
-        ArrayList<AutocompaniesBean> list = new ArrayList<>();
+    public ArrayList<BusesBean> getAll() {
+        ArrayList<BusesBean> list = new ArrayList<>();
         try(Connection connection = SingletoneConnection.getDataSource().getConnection()){
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM autocompanies");
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM buses");
             ResultSet rs = ps.executeQuery();
             while (rs.next()){
-                AutocompaniesBean tb = new AutocompaniesBean();
+                BusesBean tb = new BusesBean();
+                tb.setBusID(rs.getInt("bus_id"));
+                tb.setStateNumber(rs.getString("state_number"));
+                tb.setRun(rs.getShort("run"));
+                tb.setTecService(rs.getDate("tec_service"));
+                tb.setMarkID(rs.getInt("mark_id"));
                 tb.setAutocompanyID(rs.getInt("autocompany_id"));
-                tb.setTitle(rs.getString("title"));
-                tb.setContactNumber(rs.getShort("contact_number"));
-                tb.setPhoneNumber(rs.getString("phone_number"));
                 list.add(tb);
             }
         }catch (SQLException e){
